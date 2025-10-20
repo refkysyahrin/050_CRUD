b980a1cea0294f977b0d151104ae0d327bc5f25c
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-10-20 12:55:29 +0700

    Implementasi endpoint delete pengguna

--- index.js
+++ index.js
@@ -77,3 +77,14 @@ app.put("/api/users/:id", (req, res) => {
     }
   );
 });
+
+app.delete("/api/users/:id", (req, res) => {
+  const userId = req.params.id;
+  db.query("DELETE FROM mahasiswa WHERE id = ?", [userId], (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ message: "Database error" });
+    }
+    res.json({ message: "User deleted successfully" });
+  });
+});
